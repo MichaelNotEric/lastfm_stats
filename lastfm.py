98d6b66d9051920f1c2c80f096506407939cd3b0
Michael <michaelwehrmeister@gmail.com>
2019-02-17 21:42:16 +0000

    added counts and scrobble count

--- lastfm.py
+++ lastfm.py
@@ -13,10 +13,11 @@ def main():
     friend_stats = {}
 
     for name in names:
-        top_artist = get_top_artist(url, name, key)
-        top_album = get_top_album(url, name, key)
-        top_track = get_top_track(url, name, key)
-        friend_stats[name] = {'name': name, 'top_artist': top_artist, 'top_album': top_album, 'top_track': top_track}
+        top_artist, artists_listened = get_top_artist(url, name, key)
+        top_album, albums_listened = get_top_album(url, name, key)
+        top_track, tracks_listened = get_top_track(url, name, key)
+        total_scrobbles = get_scrobble_count(url, name, key)
+        friend_stats[name] = {'name': name, 'top_artist': top_artist, 'top_album': top_album, 'top_track': top_track, 'artists_listened': artists_listened, 'albums_listened': albums_listened, 'tracks_listened': tracks_listened, 'total_scrobbles': total_scrobbles}
 
     print friend_stats
 
@@ -37,25 +38,33 @@ def get_top_artist(url, user, key):
     artists = requests.get(url + '?method=user.gettopartists&user=' + user + '&api_key=' + key + '&format=json&limit=1').json()
 
     top_artist = artists['topartists']['artist'][0]['name']
+    artists_listened = artists['topartists']['@attr']['total']
 
-    return top_artist
+    return top_artist, artists_listened
 
 def get_top_album(url, user, key):
     """Get top album of a user"""
     albums = requests.get(url + '?method=user.gettopalbums&user=' + user + '&api_key=' + key + '&format=json&limit=1').json()
 
     top_album = albums['topalbums']['album'][0]['name'] + ' - ' + albums['topalbums']['album'][0]['artist']['name']
+    albums_listened = albums['topalbums']['@attr']['total']
 
-    return top_album
+    return top_album, albums_listened
 
 def get_top_track(url, user, key):
     """Get top track of a user"""
     tracks = requests.get(url + '?method=user.gettoptracks&user=' + user + '&api_key=' + key + '&format=json&limit=1').json()
 
     top_track = tracks['toptracks']['track'][0]['name'] + ' - ' + tracks['toptracks']['track'][0]['artist']['name']
+    tracks_listened = tracks['toptracks']['@attr']['total']
 
-    return top_track
+    return top_track, tracks_listened
 
+def get_scrobble_count(url, user, key):
+    """Get scrobble count of a user"""
+    scrobbles = requests.get(url + '?method=user.getinfo&user=' + user + '&api_key=' + key + '&format=json').json()
+
+    return scrobbles['user']['playcount']
 
 
 if __name__ == '__main__':
