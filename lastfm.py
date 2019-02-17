7ecebffd3afaea49173d32d314169632ef03f774
Michael <michaelwehrmeister@gmail.com>
2019-02-17 21:28:59 +0000

    gets top artists, albums, and tracks. changed some names

--- lastfm.py
+++ lastfm.py
@@ -10,7 +10,15 @@ def main():
     friends = get_friends(url, user, key)
     names = ['michaelnoteric'] + friends
 
-    print names
+    friend_stats = {}
+
+    for name in names:
+        top_artist = get_top_artist(url, name, key)
+        top_album = get_top_album(url, name, key)
+        top_track = get_top_track(url, name, key)
+        friend_stats[name] = {'name': name, 'top_artist': top_artist, 'top_album': top_album, 'top_track': top_track}
+
+    print friend_stats
 
 
 def get_friends(url, user, key):
@@ -24,17 +32,29 @@ def get_friends(url, user, key):
 
     return friend_names
 
-def get_top_artits():
-    """Get top artists and artist count"""
-    pass
+def get_top_artist(url, user, key):
+    """Get top artist of a user"""
+    artists = requests.get(url + '?method=user.gettopartists&user=' + user + '&api_key=' + key + '&format=json&limit=1').json()
 
-def get_top_albums():
-    """Get top albums and album count"""
-    pass
+    top_artist = artists['topartists']['artist'][0]['name']
 
-def get_top_tracks():
-    """Get top tracks and track count"""
-    pass
+    return top_artist
+
+def get_top_album(url, user, key):
+    """Get top album of a user"""
+    albums = requests.get(url + '?method=user.gettopalbums&user=' + user + '&api_key=' + key + '&format=json&limit=1').json()
+
+    top_album = albums['topalbums']['album'][0]['name'] + ' - ' + albums['topalbums']['album'][0]['artist']['name']
+
+    return top_album
+
+def get_top_track(url, user, key):
+    """Get top track of a user"""
+    tracks = requests.get(url + '?method=user.gettoptracks&user=' + user + '&api_key=' + key + '&format=json&limit=1').json()
+
+    top_track = tracks['toptracks']['track'][0]['name'] + ' - ' + tracks['toptracks']['track'][0]['artist']['name']
+
+    return top_track
 
 
 
